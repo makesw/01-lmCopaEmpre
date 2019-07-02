f0f9336b01356bd0931980f6c6e8a3f6ecf1440f
makesw <makesw@makesw.com>
2019-07-01 19:52:12 -0500

    conciliación archivo admin to colaborator

--- colaborator/templateCarnet.php
+++ colaborator/templateCarnet.php
@@ -3,7 +3,7 @@ session_start();
 if ( !isset( $_SESSION[ 'dataSession' ] ) ) {
     header( 'Location: ../index.html' );
 }else{
-    if($_SESSION[ 'dataSession' ]['perfil'] != 'colaborador'){
+    if($_SESSION[ 'dataSession' ]['perfil'] != 'admin'){
         header( 'Location: ../salir.php' );
     }
 }
@@ -25,39 +25,39 @@ $styleCarnet = mysqli_fetch_array($connect->query("select * from carnet limit 1"
 <body style="text-align: -webkit-center;font-family: Source Sans Pro, sans-serif !important;">
 	<table style="width: 340px;border: none;padding-left: 15px;border-collapse:inherit;border-spacing:0px;font-family: Source Sans Pro, sans-serif !important;">
 		<tr style="height: 50px;">
-			<td style="width: 100%;background-color: #<?php echo $styleCarnet['color_header']; ?>;" colspan="2" valign="top">
+			<td style="width: 100%;background-color: <?php echo $styleCarnet['color_header']; ?>;" colspan="2" valign="top">
 				<table>
 					<tr>
 						<td valign="top">
 							<img style="padding-left: 5px; width: 100px !important;height: 49px !important;" src="<?php echo $styleCarnet['url_logo'] ?>" alt="">
 						</td>
-						<td valign="top" style="text-align: center;padding:5px 0px 0px 2px;font-size: 14px;font-weight: bold;color:#<?php echo $styleCarnet['text_color_header']; ?>;">
+						<td valign="top" style="text-align: center;padding:5px 0px 0px 2px;font-size: 14px;font-weight: bold;color:<?php echo $styleCarnet['text_color_header']; ?>;">
 							<?php if(empty($styleCarnet['text_header'])){ echo $player['nombreComp'];} else { echo $styleCarnet['text_header'];} ?>
 						</td>
 					</tr>
 				</table>
 			</td>			
 		</tr>
-		<tr style="background-color:#<?php echo $styleCarnet['color_body'];?>;">
+		<tr style="background-color:<?php echo $styleCarnet['color_body'];?>;">
 			<td  style="width: 50%; font-weight: 600;">
 				<img style="padding:2px 0px 0px 2px; width: 105px !important;height: 110px !important;" src="<?php echo $player['url_foto'];?>" alt="">
 			</td>
 			<td style="width: 50%;font-weight: 800;text-align: left;">
 				<table style="font-size: 12px;">
 					<tr>
-						<td style="font-weight: 1000;color:#<?php echo $styleCarnet['text_color_body'];?>;">DOCUMENTO:</td>						
+						<td style="font-weight: 1000;color:<?php echo $styleCarnet['text_color_body'];?>;">DOCUMENTO:</td>						
 					</tr>
 					<tr>
 						<td><?php echo $player['documento'];?></td>
 					</tr>
 					<tr>
-						<td style="font-weight: 1000;color:#<?php echo $styleCarnet['text_color_body'];?>;">NOMBRE:</td>						
+						<td style="font-weight: 1000;color:<?php echo $styleCarnet['text_color_body'];?>;">NOMBRE:</td>						
 					</tr>
 					<tr>
 						<td><?php echo $player['nombres'].' '.$player['apellidos'];?></td>
 					</tr>
 					<tr>
-						<td style="font-weight: 1000;color:#<?php echo $styleCarnet['text_color_body'];?>;">EQUIPO:</td>					
+						<td style="font-weight: 1000;color:<?php echo $styleCarnet['text_color_body'];?>;">EQUIPO:</td>					
 					</tr>
 					<tr>
 						 <td><?php echo $player['nombreEquipo'];?></td>
@@ -66,7 +66,7 @@ $styleCarnet = mysqli_fetch_array($connect->query("select * from carnet limit 1"
 			</td>
 		</tr>
 		<tr style="height: 30px;">
-			<td colspan="2" style="font-size: 12px;padding-left: 5px;font-weight: 600;background-color: #<?php echo $styleCarnet['color_footer']; ?>;color:#<?php echo $styleCarnet['text_color_footer'];?>">
+			<td colspan="2" style="font-size: 12px;padding-left: 5px;font-weight: 600;background-color: <?php echo $styleCarnet['color_footer']; ?>;color:<?php echo $styleCarnet['text_color_footer'];?>">
 				<strong>www.copaemprendedores.com | Cel: 3002666817</strong>
 			</td>		
 		</tr>
