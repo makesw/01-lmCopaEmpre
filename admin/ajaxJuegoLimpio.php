8a59561a643d201cf6da50f3dbe0dd3b1dee0d23
makesw <makesw@makesw.com>
2020-03-02 00:19:11 -0500

    ajuste juego limpio, calcuar bien y ubicar de ultimo si no se han informado

--- admin/ajaxJuegoLimpio.php
+++ admin/ajaxJuegoLimpio.php
@@ -12,27 +12,34 @@ $idComp = isset($_GET[ 'idComp' ])?$_GET[ 'idComp' ]:0;
 $idFase = isset($_GET[ 'idFase' ])?$_GET[ 'idFase' ]:0;
 setlocale (LC_TIME,"spanish");
 date_default_timezone_set('America/Bogota');
-//Consutar equipos de la competiciòn:
-$resultEquipoPuntos = $connect->query( "select e.id, e.nombre, f.nombre fase, sum(ts.puntos) pstJL from juego j join fase f on j.id_fase = f.id and 
-f.id_competicion = ".$idComp." and  j.id_fase = ".$idFase." and j.informado = 1 join sancion s on j.id = s.id_juego join tipo_sancion ts on s.id_tipo_sancion = ts.id join 
-jugador ju on s.id_jugador = ju.id join equipo e on ju.id_equipo = e.id group by e.id order by pstJL desc");
-//generar arreglo EquipoPuntos:
+$resultEquipos = $connect->query( "select * from equipo where id_competicion = ".$idComp);
 $arrayData = array();
-while($row = mysqli_fetch_array($resultEquipoPuntos)){
-	$promedioJl = 0;
-	// por cada equipo contar partido jugados:	
-	$resultJuegos = mysqli_fetch_array($connect->query( "select count(1) juegos from juego j join equipo e on (j.id_equipo_1 = e.id or j.id_equipo_2 = e.id) 
-    and e.id = ".$row['id']." and j.informado =1 join fase f on j.id_fase = f.id and j.id_fase=".$idFase." and f.id_competicion =".$idComp));
-	if( $resultJuegos['juegos']>0){
-		$promedioJl = $row['pstJL'] / $resultJuegos['juegos'];
-	}
-	$arrayRow = array( "id" => $row['id'],"fase" => $row['fase'], "nombreEquipo" =>  $row['nombre'],"juegos" =>  $resultJuegos['juegos'],
-				  "pstJL" =>  $row['pstJL'],"prom" =>  $promedioJl);
-	array_push($arrayData,$arrayRow);
+$arrayDataCero = array();
+$arrayFinal = array();
+//Recorrer equipos calculando juego limpio de la fase seleccionada:
+while($row = mysqli_fetch_array($resultEquipos)){
+    $promedioJl = 0;
+    $cantidadJuegos = mysqli_fetch_array($connect->query( "select count(1) total, f.nombre fase from juego j join equipo e 
+    on (j.id_equipo_1 = e.id or j.id_equipo_2 = e.id) and e.id = ".$row['id']." and j.informado =1 join fase f 
+    on j.id_fase = f.id and j.id_fase=".$idFase." and f.id_competicion =".$idComp));
+   
+    $juegoLimpio = mysqli_fetch_array($connect->query( "select sum(ts.puntos) pstJL from juego j join equipo e
+    on (j.id_equipo_1 = e.id or j.id_equipo_2 = e.id) and e.id = ".$row['id']." and j.id_competicion = ".$idComp." and j.informado =1 join fase f
+    on j.id_fase = f.id and j.id_fase=".$idFase."  join sancion s on j.id = s.id_juego join tipo_sancion ts on s.id_tipo_sancion = ts.id join 
+    jugador ju on s.id_jugador = ju.id order by pstJL desc"));
+    
+    if( $cantidadJuegos['total'] > 0){
+        $promedioJl = $juegoLimpio['pstJL'] / $cantidadJuegos['total'];
+        $arrayRow = array( "id" => $row['id'],"fase" => $cantidadJuegos['fase'], "nombreEquipo" =>  $row['nombre'],"juegos" =>  $cantidadJuegos['total'],
+            "pstJL" =>  !empty($juegoLimpio['pstJL'])?$juegoLimpio['pstJL']:0,"prom" =>  $promedioJl);
+        array_push($arrayData,$arrayRow);
+    }else{
+        $arrayRow = array( "id" => $row['id'],"fase" => '', "nombreEquipo" =>  $row['nombre'],"juegos" =>  '0',
+            "pstJL" =>  '0',"prom" =>  '0');
+        array_push($arrayDataCero,$arrayRow);
+    }
 }
-
 ordenarArrayData( $arrayData );
-
 function ordenarArrayData( $arrayData ){
 	global $arrayData;
 	for( $i = 0; $i < count($arrayData) - 1; $i++)
@@ -48,8 +55,9 @@ function ordenarArrayData( $arrayData ){
             }
 	}
 }
-
+$arrayFinal = array_merge($arrayData,$arrayDataCero);
 ?>
+Tabla por fase:
 <div class="table-responsive">
 <table class="table table-striped table-bordered table-hover dataTables-JL" >
 <thead>
@@ -63,14 +71,14 @@ function ordenarArrayData( $arrayData ){
 </thead>
 <tbody>
 <?php
-for($i=0; $i<count($arrayData); $i++){
+for($i=0; $i<count($arrayFinal); $i++){
 ?>	
 <tr style="<?php if($i == 0 ){echo "background-color: #bbebba;"; } ?>">
-	<td> <?php echo $arrayData[$i]['fase']; ?>	</td>	
-	<td> <?php echo $arrayData[$i]['nombreEquipo']; ?>	</td>
-	<td> <?php echo $arrayData[$i]['juegos']; ?>	</td>
-	<td> <?php echo $arrayData[$i]['pstJL']; ?>	</td>
-	<td> <strong><?php echo round ($arrayData[$i]['prom'],2); ?></strong>	</td>
+	<td> <?php echo $arrayFinal[$i]['fase']; ?>	</td>	
+	<td> <?php echo $arrayFinal[$i]['nombreEquipo']; ?>	</td>
+	<td> <?php echo $arrayFinal[$i]['juegos']; ?>	</td>
+	<td> <?php echo $arrayFinal[$i]['pstJL']; ?>	</td>
+	<td> <strong><?php echo round ($arrayFinal[$i]['prom'],2); ?></strong>	</td>
 </tr>
 <?php } ?>
 </tbody>
